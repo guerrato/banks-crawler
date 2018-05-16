30fd4004debd5c7d59b2a016b5ca1ed6b46df975
Pedro Guerrato <pedro.guerrato@epv.network>
2018-05-16 11:04:06 -0300

    Included a method to get the banks name

--- crawler.py
+++ crawler.py
@@ -21,10 +21,19 @@ def listCountries(url):
     
     return countries
 
-def listBanks(country):
-    pass
+def listBanks(countryLink):
+    banks = dict()
+    response = requests.get(countryLink)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
+
+    bankLinks = soup.body.find(class_='wrap').find(class_='content').find(class_='mid-col').find(class_='clearfix').find_all(class_='select-list-inner')[1].find('form').find('label').find('select').find_all('option')
+
+    for b in bankLinks:
+        if (b.get('value') != ''):
+            banks[b.get_text()] = b.get('value')
+
+    return banks
 
 countries = listCountries(baseUrl)
-
-# for c in countries.keys():
-#     print(countries[c])
+print(listBanks(countries['Afghanistan']))
